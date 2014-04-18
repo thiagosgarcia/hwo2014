2ab0467627a008f5a74afdcb15526655f798a1d0
Thiago Garcia <thiagosgarcia@gmail.com>
2014-04-18 05:30:11 -0300

    Primeiros testes

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,10 @@
 var net = require("net");
 var JSONStream = require('JSONStream');
 
-var serverHost = process.argv[2];
-var serverPort = process.argv[3];
-var botName = process.argv[4];
-var botKey = process.argv[5];
+var serverHost = "testserver.helloworldopen.com";
+var serverPort = 8091;
+var botName = "Working Minds";
+var botKey = "rSOwFpIm+ddrdQ";
 
 console.log("I'm", botName, "and connect to", serverHost + ":" + serverPort);
 
@@ -26,11 +26,46 @@ function send(json) {
 jsonStream = client.pipe(JSONStream.parse());
 
 jsonStream.on('data', function(data) {
+    var info = data['data'];
+
+    if(data.msgType === 'gameInit'){
+
+    }
+    if(data.msgType === 'lapFinished')
+        console.log("Lap Finished");
+
   if (data.msgType === 'carPositions') {
-    send({
-      msgType: "throttle",
-      data: 0.5
-    });
+      //console.log(data);
+      //console.log(info);
+      var angle = parseFloat(info[0].angle + "");
+      console.log("tick " + data.gameTick + "" +
+          " | angle " + angle)
+      if(angle >40 || angle<-20){
+          send({
+              msgType: "throttle",
+              data: -0.7
+          });
+      }else if(angle >30 || angle<-15){
+          send({
+              msgType: "throttle",
+              data: -0.4
+          });
+      }else if(angle >20 || angle<-5){
+          send({
+              msgType: "throttle",
+              data: 0
+          });
+      }else if(angle > 10 || angle < -1){
+          send({
+              msgType: "throttle",
+              data: 0.4
+          });
+      }else{
+        send({
+          msgType: "throttle",
+          data: 0.9
+        });
+      }
   } else {
     if (data.msgType === 'join') {
       console.log('Joined')
@@ -38,15 +73,17 @@ jsonStream.on('data', function(data) {
       console.log('Race started');
     } else if (data.msgType === 'gameEnd') {
       console.log('Race ended');
-    } 
+    }
 
-    send({
-      msgType: "ping",
-      data: {}
-    });
+      send({
+          msgType: "ping",
+          data: {}
+      });
   }
 });
 
 jsonStream.on('error', function() {
   return console.log("disconnected");
 });
+
+
