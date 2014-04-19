228a30148337b4113bb1f04759f1577ff9caf1d6
Pedro Henrique Pereira de Jesus <pedro.henrique.108@gmail.com>
2014-04-19 14:58:39 -0300

    Reestruturação da função que escuta os eventos do socket do servidor; Adicionado a lógica de atualizar a posição do carro em Car;

--- javascript/car.js
+++ javascript/car.js
@@ -1,6 +1,24 @@
+function getCarPositionInfo(positionInfoArray) {
+	var positionInfo = {}
+	
+	for(var i = 0; i < positionInfoArray.length; i++) {
+		
+	}
+
+	return positionInfo;
+}
+
 function Car(data) {
+	this.name = data.name;
 	this.color = data.color;
 	
+	this.angle = null;
+	this.piecePosition = null;
 }
+
+Car.prototype.updateCarPosition = function(positionInfoArray) {
+  var positionInfo = getCarPositionInfo(positionInfoArray);
+  
+};
 
 module.exports = Car;
--- javascript/index.js
+++ javascript/index.js
@@ -33,85 +33,105 @@ var Car = require("./car.js");
 
 // ***** Race information objects ***** //
 var track = null;
+var myCar = null;
 
 
 // ***** Race events functions ***** //
-function gameInit(data) {
-	track = new Track(data.race.track);
-	console.log(track);
+function gameInit(info) {
+	track = new Track(info.race.track);
 	
-	pieces = data.race.track.pieces;
-	lanes = data.race.track.lanes;
+	pieces = info.race.track.pieces;
+	lanes = info.race.track.lanes;
+}
+
+function createCar(info) {
+	myCar = new Car(info);
+	console.log(myCar);
+}
+
+function ping() {
+	send({
+		msgType: "ping",
+		data: {}
+	});
+}
+
+function race(info, gameTick) {
+	myCar.updateCarPosition();
+
+	var carAngle = parseFloat(info[0].angle + "");
+	var piecePosition = info[0].piecePosition;
+	var carLane = piecePosition.lane;
+
+	//speed(piecePosition);
+	var direction = pieceDirectionString(piecePosition.pieceIndex);
+	log("tick " + gameTick + ""
+		//+" | carAngle " + carAngle
+		//+" | pieceLength " + pieceLength(piecePosition.pieceIndex, carLane)
+		//+" | isSwitch " + isSwitch(piecePosition.pieceIndex)
+		//+" | " + pieceDirectionString(piecePosition.pieceIndex)
+		+" | speed " + LastSpeed
+		+" | acc " + Acceleration
+		//+" | nextSwitch " + leftToNextSwitch(piecePosition.pieceIndex, carLane, piecePosition)
+		+" | nexTurn " + leftToNextTurn(piecePosition.pieceIndex, carLane, piecePosition)
+		+" " + ( direction  )
+		//+" " + isInTurn(piecePosition.pieceIndex))
+	);
+
+	drive(piecePosition);
 }
 
 // ***** Race events listener ***** //
 jsonStream.on('data', function(data) {
     var info = data['data'];
-
-    if(data.msgType === 'gameInit'){
-		gameInit(info);
+    
+    switch(data.msgType) {
+    	case 'join':
+    		console.log('Joined race!');
+    		ping();
+    		break;
+    	case 'gameInit':
+    		gameInit(info);
+    		ping();
+    		break;
+    	case 'yourCar':
+    		createCar(info);
+    		ping();
+    		break;
+    	case 'gameStart':
+			console.log('Race started!');
+			ping();
+			break;
+    	case 'carPositions':
+    		race(info, data['gameTick']);
+    		break;
+    	case 'lapFinished':
+    		console.log('Lap Finished');
+    		ping();
+    		break;
+    	case 'gameEnd':
+			console.log('Race ended!');
+			ping();
+			break;
+		default:
+			break;
     }
-
-    if(data.msgType === 'lapFinished')
-        log("Lap Finished");
-
-  if (data.msgType === 'carPositions') {
-      var carAngle = parseFloat(info[0].angle + "");
-      var piecePosition = info[0].piecePosition;
-      var carLane = piecePosition.lane;
-
-      //speed(piecePosition);
-        var direction = pieceDirectionString(piecePosition.pieceIndex);
-      log("tick " + data.gameTick + ""
-          //+" | carAngle " + carAngle
-          //+" | pieceLength " + pieceLength(piecePosition.pieceIndex, carLane)
-          //+" | isSwitch " + isSwitch(piecePosition.pieceIndex)
-          //+" | " + pieceDirectionString(piecePosition.pieceIndex)
-          +" | speed " + LastSpeed
-          +" | acc " + Acceleration
-          //+" | nextSwitch " + leftToNextSwitch(piecePosition.pieceIndex, carLane, piecePosition)
-          +" | nexTurn " + leftToNextTurn(piecePosition.pieceIndex, carLane, piecePosition)
-          +" " + ( direction  )
-          //+" " + isInTurn(piecePosition.pieceIndex))
-      );
-
-      drive(piecePosition);
-  } else {
-    if (data.msgType === 'join') {
-      console.log('Joined')
-    } else if (data.msgType === 'gameStart') {
-      console.log('Race started');
-    } else if (data.msgType === 'gameEnd') {
-      console.log('Race ended');
-    }
-
-      send({
-          msgType: "ping",
-          data: {}
-      });
-  }
 });
 
 jsonStream.on('error', function() {
-  return log("disconnected");
+	return log("disconnected");
 });
 
 function throttle(val){
-    if(val > 1)
-        val = 1;
-    if(val < 1)
-        val = 0;
-    log("thottle " + val);
+    if(val > 1.0)
+        val = 1.0;
+    if(val < 0.0)
+        val = 0.0;
+        
+    log("throttle " + val);
     send({
         msgType: "throttle",
         data: val
-    });
-}
-function ping(){
-    log("thottle " + val);
-    send({
-        msgType: "ping",
-        data: {}
     });
 }
 
@@ -143,37 +163,6 @@ function drive(piecePosition) {
     }
 }
 
-
-/*
-    if(carAngle >40 || carAngle<-20){
-        send({
-            msgType: "throttle",
-            data: -1
-        });
-    }else if(carAngle >30 || carAngle<-15){
-        send({
-            msgType: "throttle",
-            data: -0.7
-        });
-    }else if(carAngle >20 || carAngle<-5){
-        send({
-            msgType: "throttle",
-            data: -0.3
-        });
-    }else if(carAngle > 10 || carAngle < -1){
-        send({
-            msgType: "throttle",
-            data: 0.4
-        });
-    }else{
-        send({
-            msgType: "throttle",
-            data: 0.8
-        });
-    }
-*/
-
-
 // Funções para serem movidas para as classes correspondentes
 
 var DEBUG = true;
@@ -191,7 +180,7 @@ var lastPieceDistance = 0;
 var LastSpeed = 0;
 var Acceleration = 0;
 
-function speed(piecePosition){
+function speed(piecePosition) {
     var pieceIndex = piecePosition.pieceIndex;
     var pieceDistance = piecePosition.inPieceDistance;
     var carLane = piecePosition.lane;
@@ -214,12 +203,12 @@ function speed(piecePosition){
 // Fiz essa com calcLength, mas acho que vai ser melhor usarmos a inLaneLength
 // por que aí sim daria a real distância que o carro vai percorrer
 function pieceLength(pieceIndex, carLane){
-    if(pieceIndex == null || pieceIndex == undefined || pieces == null)
+    if(pieceIndex == null || pieceIndex == undefined)
         return 0;
-    if(pieceIndex >= pieces.length)
+    if(pieceIndex >= track.pieces.length)
         pieceIndex = 0;
 
-    var piece = pieces [pieceIndex];
+    var piece = pieces[pieceIndex];
 
     if(piece.length != undefined)
         return inLaneLength(piece.length, 0);
