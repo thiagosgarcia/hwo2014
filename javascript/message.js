89fbc3595aac270b0de47784f903830a256ce6d1
Caetano D'Araujo <cdaraujo@wkm.com.br>
2014-05-24 16:21:29 -0300

    Refactor do index.js; Criado as classes Message e Race para tratar a logica de troca de mensagens com o server durante a corrida;

---
+++ javascript/message.js
@@ -0,0 +1,128 @@
+var client;
+var race;
+
+function Message(race) {
+    this.race = race;
+
+    declarePrivateMethods(this);
+}
+
+Message.prototype.joinCustomRace = function(parameters) {
+    return this.send({
+        msgType: "joinRace",
+        data: {
+            botId: {
+                name: parameters.botName,
+                key: parameters.botKey
+            },
+            trackName: parameters.trackName,
+            password: parameters.password,
+            carCount: parameters.carCount,
+            color: parameters.color
+        }
+    });
+}
+
+Message.prototype.joinOfficialRace = function(parameters) {
+    return this.send({
+        msgType: "join",
+        data: {
+            name: parameters.botName,
+            key: parameters.botKey
+        }
+    });
+}
+
+Message.prototype.join = function(data) {
+    console.log('Joined race!');
+    this.sendPing();
+}
+
+Message.prototype.yourCar = function(data) {
+    this.race.createCar(data['data']);
+    this.sendPing();
+}
+
+Message.prototype.gameInit = function(data) {
+    this.race.init(data['data']);
+    this.sendPing();
+}
+
+Message.prototype.gameStart = function(data) {
+    console.log('Race started!');
+    this.sendPing();
+}
+
+Message.prototype.carPositions = function(data) {
+    action = this.race.run(data['data'], data['gameTick']);
+    this[action.type](action.value);
+}
+
+Message.prototype.turboAvailable = function(data) {
+    this.race.rechargeTurbo(data['data']);
+    this.sendPing();
+}
+
+Message.prototype.lapFinished = function(data) {
+    console.log('Lap finished.');
+    this.sendPing();
+}
+
+Message.prototype.gameEnd = function(data) {
+    console.log('Race ended!');
+    this.sendPing();
+}
+
+Message.prototype.unknownMessage = function(data) {
+    console.log("Unknown message: ", data);
+    this.sendPing();
+}
+
+function declarePrivateMethods(obj) {
+
+    obj.send = function(json) {
+        this.client.write(JSON.stringify(json));
+        return this.client.write('\n');
+    };
+
+    obj.sendPing = function() {
+        obj.send({
+            msgType: "ping",
+            data: {}
+        });
+    };
+
+    obj.sendThrottle = function(val) {
+        if(val > 1.0)
+            val = 1.0;
+        if(val < 0.0)
+            val = 0.0;
+
+        console.log("throttle " + val);
+
+        obj.send({
+            msgType: "throttle",
+            data: val
+        });
+    };
+
+    obj.sendSwitchLane = function(val) {
+        console.log('Will switch to ' + val + ' lane');
+
+        obj.send({
+            msgType: "switchLane",
+            data: val
+        });
+    };
+
+    obj.sendTurbo = function() {
+        obj.send({
+            msgType: "turbo",
+            data: "Geronimoooooo!!!"
+        });
+    };
+
+}
+
+module.exports = Message;
+
---
+++ javascript/race.js
@@ -0,0 +1,104 @@
+var Message = require("./message.js");
+var Track = require("./track.js");
+var Car = require("./car.js");
+
+var message;
+var ourCar;
+var ourDriver;
+
+function Race() {
+    this.message = new Message(this);
+}
+
+module.exports = Race;
+
+Race.prototype.init = function(data) {
+    track = new Track(data.race.track, data.race.raceSession);
+    ourCar.track = track;
+}
+
+Race.prototype.createCar = function(data) {
+    ourCar = new Car(data);
+    ourDriver = ourCar.driver;
+}
+
+Race.prototype.run = function(data, gameTick) {
+    ourCar.updateCarPosition(data);
+
+    var message = {
+        type: null,
+        value: null
+    };
+
+    message = decideRaceAction(gameTick);
+
+    console.log("tick " + gameTick + " : " + (Math.floor((gameTick / (60) % 100)*100) /100)  + " s"
+        +" | speed " + ourCar.lastSpeed
+        +" | acc " + ourCar.acceleration
+        +" | piece " + ourCar.currentPiece.index + " (" + ourCar.currentPiece.type + ")"
+        +" | lap " + ourCar.lap
+        +" | nextBend " + ourCar.distanceToBend()
+        //+" | lane " + ourCar.lane.index
+        //+" | switch " + ourCar.currentPiece.switch
+        //+" | Piece: lenght " + ourCar.currentPiece.lengthInLane(ourCar.track.lanes[0], ourCar.track.lanes[1])
+        //+" . radius " + ourCar.currentPiece.radius
+        //+" . angle " + ourCar.currentPiece.angle
+        //+" | nextSwitch " + leftToNextSwitch(piecePosition.pieceIndex, carLane, piecePosition)
+    );
+
+    return message;
+}
+
+Race.prototype.rechargeTurbo = function(data) {
+    ourCar.rechargeTurbo(data);
+
+    console.log("Turbo Recharged! " +
+        " | turboDurationTicks " + ourCar.turboDurationTicks +
+        " | turboFactor " + ourCar.turboFactor
+    );
+}
+
+function decideRaceAction(gameTick) {
+    action = {};
+
+    // Only check for turbo and switch sends if the game have already started
+    if(isRunning(gameTick)) {
+
+        if(shouldTurbo()) {
+            ourCar.turboAvailable = false;
+
+            action.type = 'sendTurbo';
+            return action;
+
+        } else if(shouldSwitch()) {
+            action.type = 'sendSwitchLane';
+            action.value = ourDriver.determineSwitchDirection();
+
+            if(!!action.value)
+                return action;
+        }
+
+    }
+
+    action.type = 'sendThrottle';
+    action.value = ourDriver.drive();
+
+    return action;
+}
+
+function isRunning(gameTick) {
+    return !!gameTick && !!ourCar.acceleration
+}
+
+function shouldTurbo() {
+    return ourCar.turboAvailable && ourDriver.canTurbo();
+}
+
+function shouldSwitch() {
+    if(ourDriver.checkSwitch) {
+        ourDriver.checkSwitch = false;
+        return true;
+    }
+
+    return false;
+}
