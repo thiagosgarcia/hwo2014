56a0834676e78f9e143b2e7f44a77bbc6a578ff0
Pedro Jesus <pjesus@wkm.com.br>
2014-05-31 14:43:29 -0300

    separado a lógica de determinar a direção do switch de driver para uma classe própria;

--- javascript/driver.js
+++ javascript/driver.js
@@ -1,5 +1,5 @@
-var LaneSegment = require('./laneSegment.js');
 var Piece = require('./piece.js');
+var SwitchAI = require('./switchAI.js');
 
 function Driver(car) {
 	this.car = car;
@@ -12,6 +12,8 @@ function Driver(car) {
     this.ticksBreaking = 0;
     this.lastBreakingFactors = [];
     this.speedAccelerationFactor = 49;
+
+    this.switchAI = new SwitchAI(this);
 
     declarePrivateMethods.call(this);
 }
@@ -438,118 +440,11 @@ Driver.prototype.canTurbo = function() {
 // ***** Switch intelligence ***** //
 
 Driver.prototype.determineSwitchDirection = function() {
-    var car = this.car;
-    var nextSwitchPiece = car.nextSwitchPiece;
-    if(nextSwitchPiece == null) return;
-
-    var lanes = car.track.lanes;
-    var switchesAheadToCheck = lanes.length - 1;
-    var switchesAhead = this.getSwitchesAhead(nextSwitchPiece, switchesAheadToCheck);
-
-    var segmentsToCheck = this.getSegmentsToCheck(car.track, car.lane, switchesAhead);
-    var bestLaneToSwitch = this.getBestLaneToSwitch(segmentsToCheck);
-    car.lastLane = car.lane;
-    car.nextLane = bestLaneToSwitch;
-
-    var switchDirection = this.getSwitchDirection(car.lane, bestLaneToSwitch);
-    return switchDirection;
+    return this.switchAI.determineSwitchDirection();
 };
 
 function declarePrivateMethods() {
 
-    // ***** Switch intelligence (private methods) ***** //
-
-    this.getSwitchesAhead = function(nextSwitchPiece, switchesAheadToCheck) {
-        var switchesAhead = [nextSwitchPiece];
-        var nextPiece = nextSwitchPiece;
-
-        while(switchesAheadToCheck > 0) {
-            nextPiece = nextPiece.nextPiece;
-
-            if(nextPiece.hasSwitch) {
-                switchesAhead.push(nextPiece);
-                switchesAheadToCheck--;
-            }
-        }
-
-        switchesAhead.reverse();
-        return switchesAhead;
-    };
-
-    this.getSegmentsToCheck = function(track, lane, switchesAhead) {
-        var segmentsToCheck = [new LaneSegment(lane)];
-        var laneToTheLeft = track.lanes[lane.index - 1];
-        var laneToTheRight = track.lanes[lane.index + 1];
-
-        if(!!laneToTheLeft)
-            segmentsToCheck.push(new LaneSegment(laneToTheLeft));
-
-        if(!!laneToTheRight)
-            segmentsToCheck.push(new LaneSegment(laneToTheRight));
-
-        var switchFrom = switchesAhead.pop();
-        if(switchesAhead.length <= 0) return [];
-
-        for(var i = 0; i < segmentsToCheck.length; i++) {
-            var segmentToCheck = segmentsToCheck[i];
-
-            segmentToCheck.distanceToNextSwitch = switchFrom.distanceToNextSwitch(lane, segmentToCheck.lane);
-            segmentToCheck.nextSegments = this.getSegmentsToCheck(track,
-                                                                  segmentToCheck.lane,
-                                                                  switchesAhead);
-        }
-
-        return segmentsToCheck;
-    };
-
-    this.getBestLaneToSwitch = function(segmentsToCheck) {
-        var bestLane = null;
-
-        for(var i = 0; i < segmentsToCheck.length; i++) {
-            var segmentToCheck = segmentsToCheck[i];
-            var laneToCheck = segmentToCheck.lane;
-
-            laneToCheck.bestDistance = segmentToCheck.distanceToNextSwitch;
-            laneToCheck.bestDistance += this.getBestDistanceToSwitch(segmentToCheck.nextSegments);
-
-            if(bestLane == null || bestLane.bestDistance > laneToCheck.bestDistance)
-                bestLane = laneToCheck;
-        }
-
-        return bestLane;
-    };
-
-    this.getBestDistanceToSwitch = function(segmentsToCheck) {
-        var segmentDistances = [];
-
-        for(var i = 0; i < segmentsToCheck; i++) {
-            var segmentToCheck = segmentsToCheck[i];
-            var segmentDistance = segmentToCheck.distanceToNextSwitch;
-
-            segmentDistance += this.getBestDistanceToSwitch(segmentToCheck.nextSegments);
-            segmentDistances.push(segmentDistance);
-        }
-
-        if(segmentDistances.length <= 0)
-            return 0;
-
-        return Math.min.apply(null, segmentDistances);
-    };
-
-    this.getSwitchDirection = function(currentLane, bestLane) {
-        // The best lane is more to the left of the center, switch Left.
-        if(currentLane.distanceFromCenter > bestLane.distanceFromCenter) {
-            return 'Left';
-        }
-
-        // The best lane is more to the right of the center, switch Right.
-        else if(currentLane.distanceFromCenter < bestLane.distanceFromCenter) {
-            return 'Right';
-        }
-
-        // The lane the car is driving is already the best! Nothing to do here..
-        return null;
-    }
 }
 
 module.exports = Driver;
